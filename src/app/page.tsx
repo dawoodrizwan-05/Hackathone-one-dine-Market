81b99ccd1033d30e656e9d5ff585ed0531844a1e
Muhammad Dawood Rizwan <115165513+dawoodrizwan-05@users.noreply.github.com>
2023-07-09 05:32:34 -0700

    code commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '@/views/Hero'
+import ProductList from '@/views/ProductList'
 
 export default function page() {
   return (
    <div>
     <Hero/>
+    <ProductList/>
    </div>
   )
 }
